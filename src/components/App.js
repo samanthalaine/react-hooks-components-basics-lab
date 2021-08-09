360ee91038afa6d22d32822324d81669c600c272
Samantha Laine <samanthalaine8@gmail.com>
2021-08-09 18:32:11 -0400

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,9 @@
 import React from "react";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
 
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
 
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
 
 {/* write an <About> component here */}
 
@@ -23,8 +12,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home/>
+      <About/>
     </div>
   );
 }
